8fb5bc127e04de9eeb23f09d9e9bb96f3d5a18ef
mhb717 <musharrafhassan717@gmail.com>
2024-07-26 01:44:32 +0100

    commit for adding web app interface

--- app.py
+++ app.py
@@ -23,6 +23,16 @@ def predict_api():
     print(output[0])
     return jsonify(output[0])
 
+@app.route('/predict', methods = ['POST'])
+def predict():
+    data = [float(x) for x in request.form.values()]
+    print(data)
+    new_data = scaler.transform(np.array(data).reshape(1,-1))
+    output  = model.predict(new_data)
+    
+    return render_template('home.html', prediction_text = "This is the predicted value{}".format(output[0]))
+
+
 if __name__ == "__main__":
     app.run(debug = True)
 
